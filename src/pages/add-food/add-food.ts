37ba3aad1e9d1015d49bf85e51b5d1b1b089b5bd
RaidenC <ruidongc@gmail.com>
2017-01-31 08:33:56 -0500

    add search bar, segments and FAB to add-food page

--- src/pages/add-food/add-food.ts
+++ src/pages/add-food/add-food.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
-
+import { BarcodeScanner } from 'ionic-native';
 
 @Component({
   selector: 'page-add-food',
@@ -9,13 +9,35 @@ import { NavController, NavParams } from 'ionic-angular';
 export class AddFoodPage {
 
   meal: any;
+  segment: any;
+  recentFood: Array<string>;
+  customFood: Array<string>;
+  recipes: Array<string>;
+  meals: Array<string>;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     this.meal = this.navParams.data;
+    this.segment = 'recent';
+    this.recentFood = ['egg','beef'];
+    this.customFood = ['pork meat ball','ramen'];
+    this.recipes = ['twice-cooked pork'];
+    this.meals = [];
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad AddFoodPage');
   }
 
+  scan(){
+    BarcodeScanner.scan().then((barcodeData)=>{
+      console.log(barcodeData);
+    },(err)=> {
+      console.log(err);
+    })
+  }
+
+  onInput(ev:any){
+    let val = ev.target.value;
+    console.log(val);
+  }
 }
