401f8f5c3beef5007c7d2175d498e09aee9cc686
CORPORATE\rchen <rchen@geodecisions.com>
2017-03-03 14:36:10 -0500

    search in ndb

--- src/pages/add-food/add-food.ts
+++ src/pages/add-food/add-food.ts
@@ -2,6 +2,10 @@ import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { BarcodeScanner } from 'ionic-native';
 
+import { NdbService } from '../../providers/providers';
+
+import 'rxjs/add/operator/map';
+
 @Component({
   selector: 'page-add-food',
   templateUrl: 'add-food.html'
@@ -14,30 +18,68 @@ export class AddFoodPage {
   customFood: Array<string>;
   recipes: Array<string>;
   meals: Array<string>;
+  query: string;
+  currentFoodSummary: any;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public ndbService: NdbService
+  ) {
     this.meal = this.navParams.data;
     this.segment = 'recent';
-    this.recentFood = ['egg','beef'];
-    this.customFood = ['pork meat ball','ramen'];
-    this.recipes = ['twice-cooked pork'];
+    this.recentFood = [];
+    this.customFood = [];
+    this.recipes = [];
     this.meals = [];
+    this.currentFoodSummary = this.ndbService.emptyFoodSummary();
   }
-
+  
   ionViewDidLoad() {
     console.log('ionViewDidLoad AddFoodPage');
   }
 
-  scan(){
-    BarcodeScanner.scan().then((barcodeData)=>{
+  ondrag(e: any, ndbno: any) {
+    let percent = e.getSlidingPercent();
+    if (this.currentFoodSummary.ndbno !== ndbno && percent < 0) {
+      this.getNutrientSummary(ndbno);
+    }
+  }
+
+  scan() {
+    BarcodeScanner.scan().then((barcodeData) => {
       console.log(barcodeData);
-    },(err)=> {
+    }, (err) => {
       console.log(err);
     })
   }
 
-  onInput(ev:any){
-    let val = ev.target.value;
-    console.log(val);
+  onInput(ev: any) {
+    this.query = ev.target.value;
+    this.recentFood = [];
+    this.currentFoodSummary = this.ndbService.emptyFoodSummary();
+  }
+
+  searchFood(q: any) {
+    this.ndbService.searchFood(q)
+      .subscribe(
+      value => {
+        if (value.list.item) {
+          this.recentFood = value.list.item
+        }
+      },
+      e => console.log(e),
+      () => console.log("complete")
+      );
+  }
+
+  getNutrientSummary(q: any) {
+    this.ndbService.getNutrientSummary(q).subscribe(
+      value => {
+        this.currentFoodSummary = value.report.foods[0];
+      },
+      e => console.log(e),
+      () => console.log("complete")
+    );
   }
 }
